cb6342aa95bbd8a3d5808e1de073de8c6263f46d
Abdullah Rexha <35274537+AbdullahRexha09@users.noreply.github.com>
2021-01-17 19:07:34 +0100

    Libraries added and some changes in view

--- Javascript/sketch.js
+++ Javascript/sketch.js
@@ -1,5 +1,5 @@
-var cols = 5;
-var rows = 5;
+var cols = 25;
+var rows = 25;
 
 var openSet = [];
 var closedSet = [];
@@ -14,110 +14,11 @@ var shouldDrawGrid = true;
 
 var grid = new Array(cols);
 
-function Spot(i, j) {
-    this.i = i;
-    this.j = j;
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-    this.previous = undefined;
-    this.neighbors = [];
-    this.wall = false;
-
-    if (random(1) < 0.00001) {
-        this.wall = true;
-    }
-    //click function DONE 14 December 2020
-    this.clicked = function() {
-
-        var d = dist(mouseX, mouseY, this.i * w, this.j * h);
-        if ((d < w) && this.i * w < mouseX && this.j * h < mouseY) {
-
-            if (!start || !end) {
-                if (!start & end != grid[this.i][this.j]) {
-                    noStroke();
-                    fill(color(0, 255, 0));
-                    rect(this.i * w, this.j * h, w - 1, h - 1);
-                    start = grid[this.i][this.j];
-                    if (openSet) {
-                        removeFromArray(openSet, grid[0][0]);
-                        openSet.push(start);
-                    }
-
-                }
-                if (!end & start != grid[this.i][this.j]) {
-                    noStroke();
-                    fill(color(0, 255, 0));
-                    rect(this.i * w, this.j * h, w - 1, h - 1);
-                    end = grid[this.i][this.j];
-
-                }
-
-            } else {
-                if ((start.i == this.i && start.j == this.j) || (end.i == this.i && end.j == this.j)) {
-                    if ((start.i == this.i && start.j == this.j)) {
-                        noStroke();
-                        fill(255);
-                        rect(this.i * w, this.j * h, w - 1, h - 1);
-                        start = undefined;
-                    } else if (end.i == this.i && end.j == this.j) {
-                        noStroke();
-                        fill(255);
-                        rect(this.i * w, this.j * h, w - 1, h - 1);
-                        end = undefined;
-                    }
-                } else {
-                    noStroke();
-                    fill(0);
-                    rect(this.i * w, this.j * h, w - 1, h - 1);
-                    this.wall = true;
-                }
-
-            }
-        }
-
-    }
-    this.show = function(col) {
-        fill(col);
-        if (this.wall) {
-            fill(0);
-        }
-        noStroke();
-        rect(this.i * w, this.j * h, w - 1, h - 1);
-    }
-    this.addNeighbors = function(grid) {
-        var i = this.i;
-        var j = this.j;
-        if (i < cols - 1) {
-            this.neighbors.push(grid[i + 1][j]);
-        }
-        if (i > 0) {
-            this.neighbors.push(grid[i - 1][j]);
-        }
-        if (j < rows - 1) {
-            this.neighbors.push(grid[i][j + 1]);
-        }
-        if (j > 0) {
-            this.neighbors.push(grid[i][j - 1]);
-        }
-        if (i > 0 && j > 0) {
-            this.neighbors.push(grid[i - 1][j - 1]);
-        }
-        if (i < cols - 1 && j > 0) {
-            this.neighbors.push(grid[i + 1][j - 1])
-        }
-        if (i > 0 && j < rows - 1) {
-            this.neighbors.push(grid[i - 1][j + 1]);
-        }
-        if (i < cols - 1 && j < rows - 1) {
-            this.neighbors.push(grid[i + 1][j + 1]);
-        }
-
-    }
-}
+//
 
 function setup() {
-    createCanvas(400, 400);
+    let cnv = createCanvas(400, 400);
+    cnv.parent('sketch-holder')
     console.log('A*');
 
     w = width / cols;
@@ -129,7 +30,7 @@ function setup() {
 
     for (var i = 0; i < cols; i++) {
         for (var j = 0; j < rows; j++) {
-            grid[i][j] = new Spot(i, j);
+            grid[i][j] = new Block(i, j);
         }
     }
     for (var i = 0; i < cols; i++) {
@@ -147,6 +48,7 @@ function setup() {
 }
 
 function draw() {
+    frameRate(1);
     if (shouldDrawGrid) {
         background(0);
         for (var i = 0; i < cols; i++) {
@@ -254,7 +156,7 @@ function heuristic(a, b) {
     return d;
 }
 
-function mousePressed(it) {
+function mousePressed() {
     for (var i = 0; i < cols; i++) {
         for (var j = 0; j < rows; j++) {
             grid[i][j].clicked();
@@ -262,10 +164,10 @@ function mousePressed(it) {
     }
 }
 
-function mouseDragged(it) {
-
-}
-
 function start1() {
     startPathFinder = true;
+    bootbox.alert("Your message here");
+}
+function reload(){
+    location.reload();
 }
